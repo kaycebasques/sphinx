9ebac9920b85fd70b6af98e59eb58c96cb8a8aeb
Kayce Basques <kayce@google.com>
2024-10-17 21:57:40 -0700

    remove gemini key

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ BUILD_DIR="build/sphinx"
 [[ ! -d venv ]] && python3 -m venv venv
 source venv/bin/activate
 python3 -m pip install -e .
+[[ ! -f VOYAGE_API_KEY ]] && echo "ERROR: VOYAGE_API_KEY file not found" && return 1
 export VOYAGE_API_KEY=$(cat VOYAGE_API_KEY)
-export GEMINI_API_KEY=$(cat GEMINI_API_KEY)
 sphinx-build -M html ./doc $BUILD_DIR
 deactivate
